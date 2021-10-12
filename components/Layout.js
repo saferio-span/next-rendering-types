d17984bbe714d06fb0fe1b8523c5865632d0ef54
Your Namesaferio-span <johnsaferio@dotnetethics.com>
2021-10-12 18:55:26 +0530

    Script in Head error fixed

--- components/Layout.js
+++ components/Layout.js
@@ -10,9 +10,11 @@ const Layout = ({children}) => {
                 <meta name="keywords" content="Next Demo App" />
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous" />
                 <link href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" rel="stylesheet" />
+            </Head>
+            <>
                 <Script src="https://code.jquery.com/jquery-3.6.0.min.js"></Script>
                 <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></Script>
-            </Head>
+            </>
             <main>
                 {children}
             </main>
