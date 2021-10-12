c1ca77add75823cf5eb62ac65b0d293aa055d0cf
Your Namesaferio-span <johnsaferio@dotnetethics.com>
2021-10-12 18:49:20 +0530

    script error fixed

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,6 @@
 // import style from "../styles/Layout.module.css"
 import Head from 'next/head'
+import Script from 'next/script'
 
 const Layout = ({children}) => {
     return (
@@ -9,8 +10,8 @@ const Layout = ({children}) => {
                 <meta name="keywords" content="Next Demo App" />
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous" />
                 <link href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" rel="stylesheet" />
-                <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossOrigin="anonymous"></script>
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossOrigin="anonymous"></script>
+                <Script src="https://code.jquery.com/jquery-3.6.0.min.js"></Script>
+                <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></Script>
             </Head>
             <main>
                 {children}
